df1351e4289930fded8ca2c6dfc79f2f4216cfc9
maribel huillca <maryhuillca.00@gmail.com>
2023-03-08 19:57:55 -0500

    cambios en productos

--- src/componentes/Productos/productos.js
+++ src/componentes/Productos/productos.js
@@ -1,10 +1,62 @@
 import React from 'react'
+import './productos.css'
 
 export const ProductosLista = () => {
     return (
-        <div>
-            <h1>ProductosLista</h1>
+        <>
+        <h1 className='title'>PRODUCTOS</h1>
+        <div className='productos'>
+            <div className='producto'>
+                <a href='#'>
+                    <div className="producto_img">
+                        <img src="img1.png" alt="" />
+                    </div>
+                </a>
+                <div className='producto-footer'>
+                    <h1>POLERA JOJO</h1>
+                    <p>Categoria: ropa</p>
+                    <p className='price'>$120</p>
+                </div>
+                <div className='buttom'>
+                    <div><a href='#' className='btn'>Detalles</a></div>
+                    <button className='btn'>Añadir al Carrito</button>
+                </div>
+            </div>
+            <div className='producto'>
+                <a href='#'>
+                    <div className="producto_img">
+                        <img src="img2.png" alt="" />
+                    </div>
+                </a>
+                <div className='producto-footer'>
+                    <h1>POLERA ISOKA</h1>
+                    <p>Categoria: ropa</p>
+                    <p className='price'>$90</p>
+                </div>
+                <div className='buttom'>
+                    <div><a href='#' className='btn'>Detalles</a></div>
+                    <button className='btn'>Añadir al Carrito</button>
+                </div>
+            </div>
+            <div className='producto'>
+                <a href='#'>
+                    <div className="producto_img">
+                        <img src="img3.png" alt="" />
+                    </div>
+                </a>
+                <div className='producto-footer'>
+                    <h1>POLERA LUFFY</h1>
+                    <p>Categoria: ropa</p>
+                    <p className='price'>$135</p>
+                </div>
+                <div className='buttom'>
+                    <div><a href='#' className='btn'>Detalles</a></div>
+                    <button className='btn'>Añadir al Carrito</button>
+                </div>
+            </div>
         </div>
+        </>
+
     )
 }
 
